4cec9066b18e91b37f414f36ad82c15a1fdfaf6c
yury <yurystarkov@users.noreply.github.com>
2023-12-26 00:17:59 +0700

    upd

--- info.rkt
+++ info.rkt
@@ -6,6 +6,10 @@
   '(#hash((colors
            .
            ((framework:syntax-color:scheme:string #s(background #(241 250 223)))
+            (framework:syntax-color:scheme:keyword #(0 0 9))
+            (framework:syntax-color:scheme:other #(0 0 9))
+            (framework:syntax-color:scheme:symbol  #(0 0 9))
+            (framework:syntax-color:scheme:parenthesis #(0 0 9))
             (framework:syntax-color:scheme:constant #(122 62 157))
             (framework:syntax-color:scheme:comment #s(background #(255 250 188)))))
           (name . "AlabasterBG"))))
